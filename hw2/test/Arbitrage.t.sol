93c7cd4eb7971fd23eb9f118edd8ff5f7323c366
hsutzu <elianahsu89@gmail.com>
2024-04-11 15:26:40 +0800

    init

--- hw2/test/Arbitrage.t.sol
+++ hw2/test/Arbitrage.t.sol
@@ -99,9 +99,9 @@ contract Arbitrage is Test {
         address[] memory path = new address[](5);
         path[0] = address(tokenB);
         path[1] = address(tokenC);
-        path[2] = address(tokenD)
+        path[2] = address(tokenD);
         path[3] = address(tokenE); // Adjust based on your identified arbitrage path
-        path[4] = address(tokenA)
+        path[4] = address(tokenA);
         // First, get an estimate of the output amount
         uint256[] memory amountsOutEstimate = router.getAmountsOut(amountIn, path);
 
